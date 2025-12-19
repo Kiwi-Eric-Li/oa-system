950f2f293670b8409b855009150fc69010648311
Kiwi-Eric-Li <lxf7756@126.com>
2025-12-19 16:35:46 +0800

    develop department header

--- src/components/Department/index.jsx
+++ src/components/Department/index.jsx
@@ -1,6 +1,27 @@
+import {Button} from "antd"
+import {useSelector} from "react-redux"
+
+import IconMap from "../IconMap"
+import "./index.scss"
+
 
 export default function Department(){
+
+    let collapse = useSelector(state => state.collapse.data);
+
+    const openDialog = () => {
+
+    }
+
+
     return (
-        <div>Department</div>
+        <div className="department_container">
+            <Button 
+                className={`create_department_btn ${collapse ? 'collapse' : ''}`}
+                size="small" 
+                shape="round"
+                icon={IconMap.add} 
+                onClick={openDialog}>创建</Button>
+        </div>
     )
 }
