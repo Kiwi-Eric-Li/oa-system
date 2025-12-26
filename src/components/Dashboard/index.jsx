453c1720c42d9987776550193f62d4a560415cb2
Kiwi-Eric-Li <lxf7756@126.com>
2025-12-26 16:25:06 +0800

    加入样式文件

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -13,7 +13,7 @@ export default function Dashboard(){
         {"title": "入职3年以上员工", amount: 4, styleData: {'width': '33%', 'height': '170px'}},
     ]);
     const [staffList, setStaffList] = useState({
-        "title": "工龄最老的十个人",
+        "title": "工龄最老的10个人",
         "renderList": [
             {
                 "userName": "小坏蛋",
@@ -37,6 +37,99 @@ export default function Dashboard(){
             },
         ],
         styleData: {'width': '49.8%', 'height': '350px'}
+    });
+    const [educationList, setEducationList] = useState({
+        "title": "学历情况",
+        "renderList": [
+            {
+                "name": "无学历",
+                "value": 0
+            },
+            {
+                "name": "中专",
+                "value": 1
+            },
+            {
+                "name": "大专",
+                "value": 5
+            },
+            {
+                "name": "本科",
+                "value": 11
+            },
+            {
+                "name": "研究生",
+                "value": 10
+            },
+            {
+                "name": "博士",
+                "value": 1
+            }
+        ],
+        styleData: {'width': '49.8%', 'height': '350px'}
+    });
+    const [genderList, setGenderList] = useState({
+        "title": "员工性别占比",
+        "renderList": [
+            {
+                "name": "男",
+                "value": 27.27
+            },
+            {
+                "name": "女",
+                "value": 72.73
+            }
+        ],
+        styleData: {'width': '49.8%', 'height': '350px'}
+    });
+    const [averageAge, setAverageAge] = useState({
+        "title": "平均年龄",
+        "renderList": [
+            {
+                "name": "男",
+                "value": 30.50
+            },
+            {
+                "name": "女",
+                "value": 38.56
+            }
+        ],
+        styleData: {'width': '49.8%', 'height': '350px'}
+    });
+    const [marriageCondition, setMarriageCondition] = useState({
+        "title": "员工婚姻状况",
+        "renderList": [
+            {
+                "name": "已婚",
+                "value": '50%'
+            },
+            {
+                "name": "未婚",
+                "value": '50%'
+            }
+        ],
+        styleData: {'width': '49.8%', 'height': '350px'}
+    });
+    const [ageMap, setAgeMap] = useState({
+        "title": "",
+        "renderList": [
+            {
+                "name": "20~29",
+                "value": 3
+            },
+            {
+                "name": "30~39",
+                "value": 5
+            },
+            {
+                "name": "40~49",
+                "value": 2
+            },
+            {
+                "name": "50以上",
+                "value": 3
+            }
+        ]
     })
 
 
